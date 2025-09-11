ffdefba4aa25211d14d52c4fa8132aa56e1f7ec0
Isaac Dow <isaac@merbs.ca>
2025-09-11 19:38:36 -0400

    changed some logic for how new posts appear

--- LemmyWeb/wwwroot/js/chat.js
+++ LemmyWeb/wwwroot/js/chat.js
@@ -1,52 +1,55 @@
 ﻿"use strict";
 
 function addListItem(processed) {
-    var li = document.createElement("li");
-    
+    var li = document.createElement("div");
+    li.classList = 'card mb-4';
     li.style.overflowX = "scroll";
-    document.getElementById("messagesList").prepend(li);
+    li.style.opacity = 0;
+    li.style.transition = 'opacity 0.5s ease';
+    //li.style.display = 'inline';
+    document.getElementById("msgList").prepend(li);
     let type = "post";
     switch (processed.processedType) {
         case 0:
             break;
         case 1:
-            type = "comments on";
+            type = "comments";
             break;
         case 1:
-            type = "posts about";
+            type = "posts";
             break;
     }
-    li.innerHTML = `<div id="${processed.id}" style="width:100%;transition:transform 1s ease-in;transform:scaleY(0);overflow: hidden;display:inline-block;position:relative;margin-bottom:2em;">
-    <div style="background-color:#43738e;color:white;padding:1em;">
-                 
-                    <div>${processed.extraInfo}</div>
-  
-                    </div>
-
-                    <div style="background-color:white;">
-                    <div style='border-radius:2px;border:1px solid navy;padding:2em;'>
-                                       <div style="float:right;margin-right:0.5em;" class="mr-1"> Posted: ${processed.createdDate} (UTC)</div> 
-                            
-                        
-                        <div style="border:1px solid lightgrey;padding:0.2em;background-color:#fad371"> <img src="${processed.avatarUrl || 'Lemmy_logo.svg.png'}" style="width:70px"/> 
-                                <strong>${processed.username} ${type} <a href="${processed.postUrl}">${processed.title || ''}</a>:</strong>
-                                <div style="padding:0.5em;">${processed.content}</div>
+    let innerHtml =  `           
+  <div class="card-header" style="background-color:#43738e;color:white;padding:1em;">
+      <a style="color:white;float:right;" href="${processed.postUrl}">${processed.extraInfo} (click for post)</a>
+  </div>
+  <div class="card-body" style="background-color:white;">
+      <div style="text-align:right;margin-right:0.5em;" class="mr-1"> Posted: ${processed.createdDate} (UTC)</div> 
+            <div style="padding:0.2em;background-color:white"> <img src="${processed.avatarUrl || 'Lemmy_logo.svg.png'}" style="width:70px"/> 
+            <strong>${processed.username} ${type}: </strong>
+            <div style="padding:0.5em; margin-bottom:1em;">
+                <span class="fas fa-quote-left fa-lg text-warning me-2"></span>
+                ${processed.content}
+                <span class="fas fa-quote-right fa-lg text-warning me-2" style="float:right;"></span>
+            </div>
                                
-                        </div>
+    </div>
                         
-                        <div class="p-2" style="background-color:#f7f7f1;"><strong>LemmyNanny Reported?</strong> ${processed.reason}</div>
-                        <div> <a style="float:right;" href="${processed.url}">Link on Lemmy</a></div>
-                        
-                    </div>
-                    </div>
-                    </div>
-                    `;
+    <div class="p-2" style="background-color:#f7f7f1;"><strong>LemmyNanny Reported?</strong> ${processed.reason}</div>`
+
+    if (processed.processedType === 1) {
+        innerHtml += `<div> <a style="float:right;" href="${processed.url}">View comment on Lemmy</a></div>`;
+    }
+    
+    innerHtml += `</div>`;
+
+    li.innerHTML = innerHtml;
     time = 0;
     document.getElementById('lastProcessed').innerText = `Last updated: ${time}s ago.`;
     setTimeout(() => {
-        li.firstChild.style.transform = 'scaleY(1)';
+        li.style.opacity = '1';
 
-    }, 10);
+    }, 1);
 }
 
 setInterval(() => {
