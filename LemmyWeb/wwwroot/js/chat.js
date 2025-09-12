23f0d960293c83a0883020727acd1efe09ae637e
Isaac Dow <isaac@merbs.ca>
2025-09-12 17:24:46 -0400

    ui update changes

--- LemmyWeb/wwwroot/js/chat.js
+++ LemmyWeb/wwwroot/js/chat.js
@@ -1,13 +1,13 @@
 ﻿"use strict";
 
-function addListItem(processed) {
-    var li = document.createElement("div");
+function changedFocus(processed) {
+    var li = document.getElementById("focused");
     li.classList = 'card mb-4';
     li.style.overflowX = "scroll";
     li.style.opacity = 0;
-    li.style.transition = 'opacity 0.5s ease';
+    li.style.transition = 'opacity 1s ease-in';
     //li.style.display = 'inline';
-    document.getElementById("msgList").prepend(li);
+    //document.getElementById("msgList").prepend(li);
     let type = "post";
     switch (processed.processedType) {
         case 0:
@@ -29,7 +29,9 @@ function addListItem(processed) {
 
   </div>
   <div class="card-body" style="background-color:white;">
+        <div style="text-align:right; margin-right:0.5em"><strong>${processed.processedType === 1 ? 'Comment' : 'Post'} #${processed.id}</strong></div>
       <div style="text-align:right;margin-right:0.5em;" class="mr-1" title="${new Date(processed.createdDate)}"> ${createdDate}</div> 
+
             <div style="padding:0.2em;background-color:white"> <img src="${processed.avatarUrl || 'Lemmy_logo.svg.png'}" style="width:70px"/> 
             <strong>${processed.username} ${type}: </strong>
             <div style="padding:0.5em; margin-bottom:1em;">
@@ -45,7 +47,7 @@ function addListItem(processed) {
                 <span class="fas fa-quote-right fa-lg text-warning me-2" style="float:right;"></span>
             </div>                     
     </div>        
-    <div class="p-2" style="background-color:white;border: 4px dashed ${processed.isReported ? 'darksalmon' : 'aliceblue'};"><strong>LemmyNanny Reported?</strong> ${processed.reason}</div>
+    <div class="p-2" style="background-color:white;border: 4px dashed ${processed.isReported ? 'darksalmon' : processed.reason.indexOf('Failed to check') > -1 ? '#ffe29b' : 'aliceblue'};"><strong>LemmyNanny Reported?</strong> ${processed.reason}</div>
         <div style="float:left;color:lightgrey;" data-toggle="tooltip" data-placement="top" title="${processedOn}">${processed.extraInfo}</div>`;
 
     if (processed.processedType === 1) {
@@ -57,18 +59,68 @@ function addListItem(processed) {
     innerHtml += `</div>`;
 
     li.innerHTML = innerHtml;
-    time = 0;
-    document.getElementById('lastProcessed').innerText = `Last updated: ${time}s ago.`;
+   // time = 0;
+    
     setTimeout(() => {
         li.style.opacity = '1';
 
     }, 1);
 }
 
-setInterval(() => {
-    document.getElementById('lastProcessed').innerText = `Last updated: ${++time}s ago.`;
-}, 1000);
+let checked = document.getElementById('stayCurrent').checked;
+document.getElementById('stayCurrent').addEventListener('change', () => {
+    checked = !checked;
+})
+
+function addListItem(processed) {
+    var li = document.createElement("div");
+    li.classList = 'listItem'
+    li.style.cursor = 'pointer';
+    li.style.marginRight = '1em';
+    li.style.borderRadius = '10px';
+    li.style.backgroundColor = 'white';
+    li.style.textAlign = 'center';
+    li.id = processed.id;
+    li.style.transition = 'opacity 1.1s ease-in';
+    li.style.opacity = 0;
+    document.getElementById("msgList").prepend(li);
+    time = 0;
+    li.innerHTML = `<div style="background-color:#f2eeee">${new Date(processed.processedOn).toString().split(' ')[4].split(':').slice(0, 2).join(':').toString()}</div><div style="overflow: hidden;text-overflow: ellipsis;;text-align:center;border: 4px dashed ${processed.isReported ? 'darksalmon' : processed.reason.indexOf('Failed to check') > -1 ? '#ffe29b' : 'aliceblue'};margin-right:0.1em;">
+        <div class="badge bg-secondary" style="overflow:hidden;"><i class="fas fa-solid fa-group-arrows-rotate"></i> <span>${processed.communityName}</span></div>
+        <div style="text-align:center;">
+            <div style="">
+            <div>#${processed.id}</div>
+                <div>
+                    <i class="fas fa-solid ${processed.processedType === 1 ? 'fa-comment' : 'fa-signs-post'}"></i>
+                </div>
+                <div>
+                ${processed.commentNumber}
+                </div>
+            </div>
+            
+        </div>
+    </div>`;
+
+    li.addEventListener('click', (ev) => {
+        var a = document.getElementsByClassName('listItem');
+        for (var i = 0; i < a.length; i++) {
+            a[i].style.backgroundColor = 'white';
+        }
+        li.style.backgroundColor = '#fdd063';
+        changedFocus(processed);
+    });
+
+    setTimeout(() => {
+        li.style.opacity = '1';
+
+    }, 1);
+    //document.getElementById('lastProcessed').innerText = `Last updated: ${time}s ago.`;
+}
 let time = 0;
+setInterval(() => {
+    document.getElementById('lastProcessed').innerText = `Last updated: ${time++}s ago`;
+}, 1000);
+
 var connection = new signalR.HubConnectionBuilder().withUrl("/processed").build();
 let paused = false;
 let pausedQueue = [];
@@ -83,7 +135,15 @@ connection.on("ReceiveProcessed", function (processed) {
         pauseButton.value = `Resume feed (${pausedQueue.length} new)`;
     } else {
         addListItem(processed);
-        let list = document.getElementById("messagesList");
+        if (checked) {
+            var a = document.getElementsByClassName('listItem');
+            for (var i = 0; i < a.length; i++) {
+                a[i].style.backgroundColor = 'white';
+            }
+            changedFocus(processed);
+            document.getElementById(processed.id).style.backgroundColor = '#fdd063';
+        }
+        let list = document.getElementById("msgList");
         if (list.children.length > 50) {
             list.removeChild(list.lastElementChild);
         }
@@ -93,13 +153,19 @@ connection.on("ReceiveProcessed", function (processed) {
 
 connection.on("ReceivedInitial", function (processeds) {
     console.log("proceeds");
+    var a = document.getElementsByClassName('listItem');
+
     for (var i = 0; i < processeds.length; i++) {
         addListItem(processeds[i]);
     }
+    let showProc = processeds[processeds.length - 1];
+    changedFocus(showProc);
+    document.getElementById(showProc.id).style.backgroundColor = '#fdd063';
+
 });
 
 connection.on("ViewerCount", function (viewcount) {
-    document.getElementById('numWatcher').innerText = `${viewcount - 1} sailors sailing wit ye.`;
+    document.getElementById('numWatcher').innerText = `${viewcount - 1} sailors sailing wit ye`;
 });
 
 //connection.start().then(function () {
@@ -126,6 +192,7 @@ pauseButton.addEventListener("click", function (event) {
 
 var startTimeout = null;
 var retryTimeout = null;
+var tryCount = 0;
 
 async function start() {
     try {
