bf433d75a1bb789d5b2cbf012101101a000e473f
Isaac Dow <isaac@merbs.ca>
2025-09-13 14:38:26 -0400

    trying to add App Insights

--- LemmyWeb/Program.cs
+++ LemmyWeb/Program.cs
@@ -7,7 +7,7 @@ namespace LemmyWeb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddApplicationInsightsTelemetry();
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddSignalR();
