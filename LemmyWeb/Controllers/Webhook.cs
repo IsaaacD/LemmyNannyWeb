4e1e39aa0716f5cc6c16a61a67aac09aa68ee423
Isaac Dow <isaac@merbs.ca>
2025-09-21 12:52:01 -0400

    trying string no FromBody

--- LemmyWeb/Controllers/Webhook.cs
+++ LemmyWeb/Controllers/Webhook.cs
@@ -51,7 +51,7 @@ namespace LemmyWeb.Controllers
 
         [Route("comment")]
         [HttpPost]
-        public async Task CommentBodyFromLemmy(JsonObject data)
+        public async Task CommentBodyFromLemmy(string data)
         {
             var converted = JsonSerializer.Serialize(data);
             var memoryProcessed = new List<string>();
